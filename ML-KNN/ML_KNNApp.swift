7dc800f3c17afda72ec6db9f7f7bcb52b7dc1ff5
Nathália Cardoso <crds.nathalia@gmail.com>
2021-06-23 09:29:38 -0300

    initial commit

---
+++ ML-KNN/ML_KNNApp.swift
@@ -0,0 +1,17 @@
+//
+//  ML_KNNApp.swift
+//  ML-KNN
+//
+//  Created by Nathalia Cardoso on 23/06/21.
+//
+
+import SwiftUI
+
+@main
+struct ML_KNNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
